feae9b804ed852a77cdc121327b273e829a83056
sebe324 <sebeq05@gmail.com>
2023-04-29 15:10:50 +0200

    Added a basic ui and input output images

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
 
 using namespace cv;
 
 
-void treshold(Mat& I, Mat& O, unsigned int t){
+void treshold(Mat I, Mat& O, unsigned int t){
 
 CV_Assert(I.depth()==CV_8U);
 CV_Assert(O.depth()==CV_8U);
@@ -28,7 +30,7 @@ uchar* pOutput;
 	}	
 }
 
-void absoluteMotion(Mat& I, Mat& prevI, Mat& O){
+void absoluteMotion(Mat I, Mat prevI, Mat& O){
 
 CV_Assert(I.depth()==CV_8U);
 CV_Assert(prevI.depth()==CV_8U);
@@ -56,7 +58,7 @@ for(int y=0; y<nRows; y++){
 }
 }
 
-void temporalFiltering(Mat& I, Mat& O, float t){
+void temporalFiltering(Mat I, Mat& O, float t){
 	CV_Assert(I.depth()==CV_8U);
 	CV_Assert(O.depth()==CV_8U);
 	if(O.channels()!=1)
@@ -81,11 +83,11 @@ void temporalFiltering(Mat& I, Mat& O, float t){
 	
 }
 
-void convolution(Mat& I, Mat& O, Mat& K){
+void convolution(Mat I, Mat& O, Mat& K){
 
 }
 char* characters=".'`^\",:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-void asciify(Mat& I, Mat& O){
+void asciify(Mat I, Mat& O){
 CV_Assert(I.depth()==CV_8U);
 CV_Assert(O.depth()==CV_8U);
 
@@ -108,7 +110,7 @@ uchar* pTmp;
 for(int y=0; y<nRowsTmp; y++){
 	pTmp=tmp.ptr<uchar>(y);
 	for(int x=0; x<nColsTmp; x++){
-		putText(O,std::string(1,characters[int(pTmp[x]/3.7)]),Point(x*10,y*10), FONT_HERSHEY_SIMPLEX ,0.5,(255,255,255),1);
+		putText(O,std::string(1,characters[int(pTmp[x]/3.7)]),Point(x*5,y*5), FONT_HERSHEY_SIMPLEX ,0.25,(255,255,255),1);
 	}
 }
 
@@ -116,18 +118,54 @@ for(int y=0; y<nRowsTmp; y++){
 
 int main()
 {
-
+	//create window
+	sf::RenderWindow window(sf::VideoMode(1600,1000), "elo");
+	
+	//sfml objects for displaying
+	sf::Image inputImage;
+	sf::Texture inputTexture;
+	sf::Sprite inputSprite;
+	sf::Image outputImage;
+	sf::Texture outputTexture;
+	sf::Sprite outputSprite;
+	
+	//font
+	sf::Font font;
+	if(!font.loadFromFile("font.ttf")){
+	std::cout<<"COULD NOT OPEN FONT";
+	return -1;
+	}
+	sf::Text info;
+	info.setFont(font);
+	info.setString( std::string("0. None\n")+
+			std::string("1. Treshold\n")+
+	 		std::string("2. Absolute Motion\n")+
+	 		std::string("3. Temporal Filtering\n")+
+	 		std::string("4. Convolution\n")+
+	 		std::string("5. Sobel Edge\n")+
+	 		std::string("6. Morphological\n")+
+	 		std::string("7. Median\n")+
+	 		std::string("8. Adaptive Treshold\n")+
+	 		std::string("9. Asciify")
+	 		);
+	info.setPosition(50.f,400.f);
+	 		
+	//capture camera
 	VideoCapture cam;
 	cam.open(0);
-	namedWindow("Window", WINDOW_AUTOSIZE);
 	if(!cam.isOpened()){
 		std::cout<<"COULD NOT FIND A CAMERA";
 		return -1;
 	}
+	
+	//opencv Matrices
 	Mat inputFrame;
 	Mat previousFrame;
 	Mat outputFrame;
 	Mat idkFrame;
+	
+	
+	//enum that represents current image processing mode
 	enum{
 	NONE='0',
 	TRESHOLD,
@@ -141,44 +179,76 @@ int main()
 	ASCIIFY,
 	};
 	
-	char key=TRESHOLD;
-	cam>>inputFrame;
-	outputFrame=inputFrame.clone();
-	resize(outputFrame,idkFrame,Size(inputFrame.cols*2,inputFrame.rows*2));
-	
-	while(true){
-	cam>>inputFrame;
-	char input=waitKey(25);
-	if(input!=-1) key=input;
-	switch(key){
+	char key=NONE;
+
+	outputSprite.setPosition(700.f,0.f);
+	while(window.isOpen()){
+		sf::Event event;
+		while(window.pollEvent(event)){
+			if(event.type==sf::Event::Closed){
+			window.close();
+			}
+			else if(event.type==sf::Event::TextEntered){
+			if(event.text.unicode>='0'&& event.text.unicode<='9')
+				key=event.text.unicode;
+				
+			}
+		}
+		
+		cam>>inputFrame;
+		switch(key){
+		case NONE:
+			outputFrame=inputFrame.clone();
+		break;
 		case TRESHOLD:
-		treshold(inputFrame,outputFrame,127);
-		resize(outputFrame,idkFrame,Size(inputFrame.cols*2,inputFrame.rows*2));
+			treshold(inputFrame.clone(),outputFrame,125);
 		break;
 		case ABSOLUTE_MOTION:
-			absoluteMotion(inputFrame,previousFrame,outputFrame);
-			resize(outputFrame,idkFrame,Size(inputFrame.cols*2,inputFrame.rows*2));
+			absoluteMotion(inputFrame.clone(),previousFrame,outputFrame);
 		break;
 		case TEMPORAL_FILTERING:
-			temporalFiltering(inputFrame,outputFrame,0.1f);
-			resize(outputFrame,idkFrame,Size(inputFrame.cols*2,inputFrame.rows*2));
+			temporalFiltering(inputFrame.clone(),outputFrame,0.3);
+		break;
+		case CONVOLUTION:
+		
+		break;
+		case SOBEL_EDGE:
+		
+		break;
+		case MORPHOLOGICAL:
+		
+		break;
+		case MEDIAN:
+		
+		break;
+		case ADAPTIVE_TRESHOLD:
+		
 		break;
 		case ASCIIFY:
-		asciify(inputFrame,idkFrame);
+			asciify(inputFrame.clone(),outputFrame);
 		break;
-		case NONE:
-			cam>>outputFrame;
-			resize(outputFrame,idkFrame,Size(inputFrame.cols*2,inputFrame.rows*2));
+		default:
+		
 		break;
-		case 'x':
-			return -1;
-		break;
+		}
+		if(inputFrame.channels()==3) cvtColor(inputFrame,inputFrame,COLOR_BGR2RGBA);
+		else cvtColor(inputFrame,inputFrame,COLOR_GRAY2RGBA);
+		if(outputFrame.channels()==3) cvtColor(outputFrame,outputFrame,COLOR_BGR2RGBA);
+		else cvtColor(outputFrame,outputFrame,COLOR_GRAY2RGBA);
+		inputImage.create(inputFrame.cols,inputFrame.rows,inputFrame.ptr());
+		outputImage.create(outputFrame.cols,outputFrame.rows,outputFrame.ptr());
+		if(!inputTexture.loadFromImage(inputImage)) break;
+		if(!outputTexture.loadFromImage(outputImage)) break;
+		inputSprite.setTexture(inputTexture);
+		outputSprite.setTexture(outputTexture);
+		
+		window.clear(sf::Color(80,80,80));
+		window.draw(inputSprite);
+		window.draw(outputSprite);
+		window.draw(info);
+		window.display();
+		
+		cam>>previousFrame;
 	}
-	imshow("Window",idkFrame);
-	previousFrame=inputFrame.clone();
-	}
-
-
-	waitKey(0);
 	return 0;
 }
